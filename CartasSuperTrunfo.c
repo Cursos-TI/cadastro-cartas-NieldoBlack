d03be3e1aae5235afa78605aa4ea317906899978
NieldoBlack <daniel.silva.carneiro2014@gmail.com>
2025-12-09 15:17:22 +0000

    Desafio novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,20 +7,23 @@
 int main() {
   
 int populaçao,pontos; // Área para definição das variáveis para armazenar as propriedades das cidades
-
 float area, pib;
-char codigo [50];
+char codigo [5];
 char nome [50];
-char estado = 'A';
+char estado;
 
-printf("Digite seu estado: \n");  // Área para entrada de dados
-scanf("%s", &estado);
+printf("- Cadastro de cartas super trunfo -\n");
 
-printf("Digite o código da carta: \n");
+printf("\n"); // Para pular uma linha
+
+printf("Digite a letra do estado (A-H): \n");  // Área para entrada de dados
+scanf(" %c", &estado); // Espaço antes do %c para não dar erro de leitura 
+
+printf("Digite o código da carta (ex: A01): \n");
 scanf("%s", &codigo);
 
 printf("Digite o nome da cidade: \n");
-scanf("%s", &nome);
+scanf(" %[^\n]", &nome); // le o nome até o fim da linha (com espaços) 
 
 printf("Digite o numero de população: \n");
 scanf("%d", &populaçao);
@@ -33,6 +36,8 @@ scanf("%f", &pib);
 
 printf("Digite o numero de pontos turisticos: \n");
 scanf("%d", &pontos);
+
+printf("\n - Dados da carta - \n");
 
 printf("\n");
 
@@ -40,46 +45,10 @@ printf("Carta 1:\n");
 printf("Estado: %c\n", estado); // Área para exibição dos dados da cidade
 printf("Código: %s\n", codigo);
 printf("Cidade: %s\n", nome);
-printf("População: %d\n", populaçao);
+printf("População: %d habitantes\n", populaçao);
 printf("Área: %.2f Km²\n", area);
 printf("PIB: %.2f Bilhoes de reais\n", pib);
 printf("Pontos turísticos: %d\n", pontos);
-
-printf("\n");
-
-printf("Digite seu estado: \n");  // Área para entrada de dados
-scanf("%s", &estado);
-
-printf("Digite o código da carta: \n");
-scanf("%s", &codigo);
-
-printf("Digite o nome da cidade: \n");
-scanf("%s", &nome);
-
-printf("Digite o numero de população: \n");
-scanf("%d", &populaçao);
-
-printf("Digite a área(em km²): \n");
-scanf("%f", &area);
-
-printf("Digite o PIB: \n");
-scanf("%f", &pib);
-
-printf("Digite o numero de pontos turisticos: \n");
-scanf("%d", &pontos);
-
-printf("\n");
-
-printf("Carta 2:\n");
-printf("Estado: %c\n", estado); // Área para exibição dos dados da cidade
-printf("Código: %s\n", codigo);
-printf("Cidade: %s\n", nome);
-printf("População: %d\n", populaçao);
-printf("Área: %.2f Km²\n", area);
-printf("PIB: %.2f Bilhoes de reais\n", pib);
-printf("Pontos turísticos: %d\n", pontos);
-
-printf("\n");
 
 return 0;
 } 
